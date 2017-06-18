7bbad532b1b6445437ceb08a260fc407439db92b
openmuthu <openmuthu@yahoo.co.in>
2017-06-18 16:21:19 +0530

    To fix a crash

--- UnicodeSearch/unicodesearch/src/main/java/org/jaya/util/Utils.java
+++ UnicodeSearch/unicodesearch/src/main/java/org/jaya/util/Utils.java
@@ -33,6 +33,8 @@ public class Utils {
 	public static final String removeExtension(final String filename){
 		if (filename == null) return null;
 		int dotIndex = filename.lastIndexOf(".");
+		if( dotIndex <= 0 )
+			return filename;
 		return filename.substring(0, dotIndex);
 	}
 
