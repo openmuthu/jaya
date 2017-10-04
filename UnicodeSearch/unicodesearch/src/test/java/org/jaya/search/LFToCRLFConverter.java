75e037c2eb6e8a094776a0ae38b1a6503db71545
openmuthu <openmuthu@yahoo.co.in>
2017-10-04 21:15:47 +0530

    LFToCRLFConverter

---
+++ UnicodeSearch/unicodesearch/src/test/java/org/jaya/search/LFToCRLFConverter.java
@@ -0,0 +1,45 @@
+package org.jaya.search;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.jaya.util.Constatants;
+import org.jaya.util.PathUtils;
+import org.jaya.util.Utils;
+
+public class LFToCRLFConverter {
+	public static void main(String[] args){
+		List<File> files = FileUtils.getListOfFiles(new File(Constatants.FILES_TO_INDEX_DIRECTORY));
+		//List<File> files = FileUtils.getListOfFiles(new File(PathUtils.get(Constatants.FILES_TO_INDEX_DIRECTORY, "harikathAmRtasAra")));
+		for(File file:files){
+			String path = file.getAbsolutePath();
+			if( !Utils.getFileExtension(path).equals("txt") )
+				continue;
+			String encoding = Utils.guessFileEncoding(path);
+			if( encoding.equals("UTF-8") ){
+				try{
+					StringBuilder sb = new StringBuilder();
+					
+					BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+					String line = reader.readLine();
+					sb.append(line);
+					for(;(line=reader.readLine())!=null;){
+						sb.append("\r\n"+line);
+					}
+					reader.close();
+					org.apache.commons.io.FileUtils.writeStringToFile(file, sb.toString(), "UTF-8");
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+				
+			}
+		}
+	}
+}
